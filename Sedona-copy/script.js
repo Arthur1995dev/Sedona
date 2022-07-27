c4dde94bd1dd82958fa037bdb2991dc7a89c5310
Arthur1995dev <h1f728jri3@yandex.ru>
2022-07-27 14:23:55 +0400

    Первая проверка

---
+++ Sedona-copy/script.js
@@ -0,0 +1,9 @@
+const burgerButton = document.querySelector('.burger_container');
+const burgerLine = document.querySelector('.burger_line');
+const burgerMenu = document.querySelector('.burger_menu_container');
+
+
+burgerButton.addEventListener('click', function() {
+    burgerButton.classList.toggle('_active');
+    burgerMenu.classList.toggle('hidden');
+})
